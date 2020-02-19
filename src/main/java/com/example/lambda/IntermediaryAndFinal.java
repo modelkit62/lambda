e7f9e0044a69e9959df714e44aae243717b9c249
Daniel <modelkit62@gmail.com>
2020-02-19 23:35:33 +0000

    Initial commit

---
+++ src/main/java/com/example/lambda/IntermediaryAndFinal.java
@@ -0,0 +1,32 @@
+package com.example.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class IntermediaryAndFinal{
+
+    public static void main(String[] args) {
+
+        List<String> lista = new ArrayList<>();
+        lista.add("one");
+        lista.add("two");
+        lista.add("three");
+
+        System.out.println(lista.size());
+
+        Predicate<String> p1 = Predicate.isEqual("two");
+        Predicate<String> p2 = Predicate.isEqual("three");
+
+        List<String> list = new ArrayList<>();
+
+        lista.stream()
+                .peek(System.out::println)
+                .filter(p1.or(p2))
+                .forEach(list::add);
+
+        System.out.println("size list = " + list.size());
+    }
+
+}
